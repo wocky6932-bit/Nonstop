8fceb9712cf44e078549b855b69071b6c8d21825
mactar27 <ndiayeamadoumactar3@gmail.com>
2026-08-23 11:35:52 +0000

    style: make home page header transparent overlaying the hero

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function HomePage() {
     <div className="min-h-screen bg-white text-black" suppressHydrationWarning>
 
       {/* Header */}
-      <header className="sticky top-0 z-50 bg-black border-b border-white/10">
+      <header className="absolute top-0 left-0 right-0 z-50 bg-transparent border-b border-white/10">
         <div className="container mx-auto px-4 lg:px-8">
           <div className="flex items-center justify-between py-4">
             {/* Menu Hamburger */}
